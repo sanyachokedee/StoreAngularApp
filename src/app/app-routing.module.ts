2fb9c895e485f43f59a09be0bf36bb64ac3018fc
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-12-10 13:20:29 +0700

    update 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontendLayoutComponent } from './layouts/frontend-layout/frontend-layout.component';
+import { AboutComponent } from './pages/frontend/about/about.component';
+import { HomeComponent } from './pages/frontend/home/home.component';
+import { ContactComponent } from './pages/frontend/contact/contact.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { ForgotpassComponent } from './pages/auth/forgotpass/forgotpass.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // Route สำหรับหน้า frontend layout
+  {
+    path: '',
+    component: FrontendLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      }
+    ]
+  },
+
+  // Route สำหรับเรียกหน้า Auth Layout
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'forgotpass',
+        component: ForgotpassComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
