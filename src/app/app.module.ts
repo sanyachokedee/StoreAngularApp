f62eb485bb8e2c6dba7fb9c20dc3238c2e148f84
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-12-10 08:14:21 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontendLayoutComponent } from './layouts/frontend-layout/frontend-layout.component';
+import { BackendLayoutComponent } from './layouts/backend-layout/backend-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { HomeComponent } from './pages/frontend/home/home.component';
+import { AboutComponent } from './pages/frontend/about/about.component';
+import { ContactComponent } from './pages/frontend/contact/contact.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { ForgotpassComponent } from './pages/auth/forgotpass/forgotpass.component';
+import { DashboardComponent } from './pages/backend/dashboard/dashboard.component';
+import { StockComponent } from './pages/backend/stock/stock.component';
+import { ReportComponent } from './pages/backend/report/report.component';
+import { UsersComponent } from './pages/backend/users/users.component';
+import { SettingComponent } from './pages/backend/setting/setting.component';
+import { HeaderFrontendComponent } from './shared/frontend/header-frontend/header-frontend.component';
+import { FooterFrontendComponent } from './shared/frontend/footer-frontend/footer-frontend.component';
+import { HeaderBackendComponent } from './shared/backend/header-backend/header-backend.component';
+import { FooterBackendComponent } from './shared/backend/footer-backend/footer-backend.component';
+import { SidebarBackendComponent } from './shared/backend/sidebar-backend/sidebar-backend.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontendLayoutComponent,
+    BackendLayoutComponent,
+    AuthLayoutComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotpassComponent,
+    DashboardComponent,
+    StockComponent,
+    ReportComponent,
+    UsersComponent,
+    SettingComponent,
+    HeaderFrontendComponent,
+    FooterFrontendComponent,
+    HeaderBackendComponent,
+    FooterBackendComponent,
+    SidebarBackendComponent
   ],
   imports: [
     BrowserModule,
